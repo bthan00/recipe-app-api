48f1d48ad7e24df8dd0178a6ef0c6eed6e7b0d17
bthan-daa <113547400+bthan-daa@users.noreply.github.com>
2022-09-19 17:08:05 -0400

    fix all testing

--- app/app/tests.py
+++ app/app/tests.py
@@ -1,16 +1,3 @@
 """
 Sample Tests
 """
-
-from django.test import SimpleTestCase
-from app import calc
-
-class CalcTests(SimpleTestCase):
-    """Test the calc module"""
-    
-    def test_add_numbers(self):
-        res = calc.add(5,6)
-        self.assertEqual(res, 11)
-    
-    def test_subtract_numbers(self):
-        self.assertEqual(calc.subtract(10,5), 5)
--- app/core/management/commands/wait_for_db.py
+++ app/core/management/commands/wait_for_db.py
@@ -7,10 +7,12 @@ from psycopg2 import OperationalError as Psycopg2OpError
 from django.db.utils import OperationalError
 from django.core.management.base import BaseCommand
 
+
 class Command(BaseCommand):
-    """Django command to wait for db"""
+    """  Django command to wait for db  """
+
     def handle(self, *args, **options):
-        """Entrypoint for command."""
+        """ Entrypoint for command. """
         self.stdout.write("Waiting for database...")
         db_up = False
         while db_up is False:
@@ -20,4 +22,5 @@ class Command(BaseCommand):
             except (Psycopg2OpError, OperationalError):
                 self.stdout.write("Database unavailable, waiting 1 second...")
                 time.sleep(1)
+
         self.stdout.write(self.style.SUCCESS("Database Available!"))
