ca200ccb3303318bf900d961d3d6bed2588827fc
bthan-daa <bill.than@ontario.ca>
2022-09-21 14:30:41 -0400

    testing;

--- app/app/settings.py
+++ app/app/settings.py
@@ -38,9 +38,11 @@ INSTALLED_APPS = [
     'django.contrib.sessions',
     'django.contrib.messages',
     'django.contrib.staticfiles',
-    'core',
+
     'rest_framework',
     'drf_spectacular',
+    'core', 
+    'user',
 ]
 
 MIDDLEWARE = [
@@ -133,6 +135,6 @@ DEFAULT_AUTO_FIELD = 'django.db.models.BigAutoField'
 
 AUTH_USER_MODEL = 'core.User'
 
-REST_FRAMEWORk = {
+REST_FRAMEWORK = {
     'DEFAULT_SCHEMA_CLASS': 'drf_spectacular.openapi.AutoSchema',
 }
